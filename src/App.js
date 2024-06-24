9373285d03f045255f8f09cbce983a31370deb53
YiTing07 <hevecandy@gmail.com>
2024-06-24 20:42:33 +0800

    build: add HomePage.jsx & HomePage.module.scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './reset.css';
 import './App.css';
 import Header from './components/Header/Header';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
+      <HomePage/>
+      
     </div>
   );
 }
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import styles from './HomePage.module.scss'
+
+export default function HomePage() {
+  return (
+    <section className={styles.homePage}>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <h1>Hello! I'm</h1>
+          <h1>Eve Hung.</h1>
+        </div>
+      </div>
+    </section>
+  )
+}

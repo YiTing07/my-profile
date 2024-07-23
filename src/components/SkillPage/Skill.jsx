b9aca9962c5f5adaefd4467d85cb6b848ecb1f25
YiTing07 <hevecandy@gmail.com>
2024-07-23 23:20:06 +0800

    fix: add description in Skill Page, and Simplify the structure

--- src/components/SkillPage/Skill.jsx
+++ src/components/SkillPage/Skill.jsx
@@ -2,6 +2,62 @@ import styles from './Skill.module.scss';
 import codeIcon from '../../assets/icons/code.png';
 import toolIcon from '../../assets/icons/tool.png';
 
+const professional = [
+  "擁有 HTML、CSS、JavaScript 知識功底",
+  "熟悉 CSS 進階排版 (Flexbox、Grid)",
+  "熟悉 Bootstrap 框架應用",
+  "熟悉 SASS / SCSS 框架應用",
+  "熟悉 RWD 響應式網頁設計",
+  "熟悉 React.js 開發框架",
+  "了解 Git 版本控制",
+  "了解 Node.js、Express.js"
+]
+
+const tools = [
+  "Figma：製作框線稿、邏輯流程等",
+  "Canva：製作與編輯文宣",
+  "Photoshop：編輯相片、設計文宣",
+  "Trello：利用看板列出目前所需完成的任務項目",
+  "Asana：使用專案清單、甘特圖、看板等模式，完成個人網站的任務目標"
+]
+
+function Professional() {
+  return (
+    <div className={`${styles.skillCard} professional`}>
+      <div className={styles.icon}>
+        <img src={codeIcon} alt="codeIcon" />
+      </div>
+      <div className={styles.skillContent}>
+        <h3>基礎語言與前端框架</h3>
+        <div className={styles.skillList}>
+          <ul>
+            {professional.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+function Tools() {
+  return (
+    <div className={`${styles.skillCard} tool`}>
+      <div className={styles.icon}>
+        <img src={toolIcon} alt="toolIcon" />
+      </div>
+      <div className={styles.skillContent}>
+        <h3>工具使用</h3>
+        <div className={styles.skillList}>
+          <ul>
+            {tools.map((item, index) => <li key={index}>{item}</li>)}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function Skill() {
   return (
     <section id='Skill' className={styles.skillPage}>
@@ -9,48 +65,8 @@ export default function Skill() {
         <div className={styles.skill}>
           <h2>SKILL</h2>
           <div className={styles.skillCards}>
-            <div className={`${styles.skillCard} professional`}>
-              <div className={styles.icon}>
-                <img src={codeIcon} alt="codeIcon" />
-              </div>
-              <div className={styles.skillContent}>
-                <h3>基礎語言與前端框架</h3>
-                <div className={styles.skillList}>
-                  <ul>
-                    <li>擁有 HTML、CSS、JavaScript 知識功底</li>
-                    <li>熟悉 CSS 進階排版 (Flexbox、Grid)</li>
-                    <li>熟悉 Bootstrap 框架應用</li>
-                    <li>熟悉 SASS / SCSS 框架應用</li>
-                  </ul>
-                  <ul>
-                    <li>熟悉 RWD 響應式網頁設計</li>
-                    <li>熟悉 React.js 開發框架</li>
-                    <li>了解 Git 版本控制</li>
-                    <li>了解 Node.js、Express.js</li>
-                  </ul>
-                </div>
-                
-              </div>
-            </div>
-            <div className={`${styles.skillCard} tool`}>
-              <div className={styles.icon}>
-                <img src={toolIcon} alt="toolIcon" />
-              </div>
-              <div className={styles.skillContent}>
-                <h3>工具使用</h3>
-                <div className={styles.skillList}>
-                  <ul>
-                    <li>Figma</li>
-                    <li>Canva</li>
-                    <li>Photoshop</li>
-                  </ul>
-                  <ul>
-                    <li>Trello</li>
-                    <li>Asana</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            <Professional />
+            <Tools/>
           </div>
         </div>
       </div>
